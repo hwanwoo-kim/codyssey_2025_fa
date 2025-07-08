62082d6d6f5d50856f33e548f2cdc1de917ab82a
hwanwoo <hwan25441144@gmail.com>
2025-07-08 10:05:14 +0900

    Update sonarqube_test_py_0701.py

--- sonarqube_test_py_0701.py
+++ sonarqube_test_py_0701.py
@@ -14,3 +14,37 @@ def execute_user_code():
 
 authenticate()
 execute_user_code()
+
+
+
+# [버그 2개]: Division by zero 가능성, 잘못된 리스트 인덱스 사용
+
+def divide(a, b):
+    return a / b  # [버그1] b가 0일 때 ZeroDivisionError
+
+def get_item(lst, index):
+    return lst[index + 1]  # [버그2] 범위를 벗어난 인덱스 접근 가능성
+
+divide(10, 0)
+get_item([1, 2, 3], 2)
+
+
+
+
+# [코드 스멜 2개]: 중복된 조건문, 의미 없는 변수명 사용
+
+def process(data):
+    if data == "yes":
+        print("You said yes")
+    elif data == "yes":  # [스멜1] 중복된 조건
+        print("Still yes")
+
+def calc():
+    x = 1
+    y = 2
+    z = x + y  # [스멜2] 의미 없는 변수명 사용
+    print(z)
+
+process("yes")
+calc()
+
